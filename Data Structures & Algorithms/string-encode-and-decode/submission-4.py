06a8caf842bb203900db8386e410215f436ba1b6
Amanzgr8 <95392002+Amanzgr8@users.noreply.github.com>
2026-09-20 17:07:14 -0500

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,14 @@
+import base64
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if strs == []:
+            return ""
+        else:
+            parts = [base64.b64encode(s.encode()).decode() + ","  for s in strs]
+            joined = "".join(parts)
+            return joined
+
+    def decode(self, s: str) -> List[str]:
+        back = [base64.b64decode(p).decode() for p in s.split(",")[:-1] ]
+        return back
